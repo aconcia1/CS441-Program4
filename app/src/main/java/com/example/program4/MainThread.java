3544be20dbdc226931687e8b6ee2d7afa720a02b
aconcia1 <conciatorianthony@gmail.com>
2019-03-09 22:28:54 -0500

    Added the gamescene frame work and mainthread handler

---
+++ app/src/main/java/com/example/program4/MainThread.java
@@ -0,0 +1,11 @@
+package com.example.program4;
+
+import android.view.SurfaceHolder;
+
+public class MainThread extends Thread {
+
+    public MainThread(SurfaceHolder surfaceHolder, GamePanel gamePanel) {
+
+
+    }
+}
